3fc318509ac97c784daed83c9ffad6a8fc82c251
LucasAlberto0 <lucasalberto4321@gmail.com>
2024-08-26 23:20:49 -0300

    fix: about-page-string

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -3,11 +3,13 @@ import { HeaderComponent } from '../../components/header/header.component';
 import { PhotoComponent } from '../../components/photo/photo.component';
 import { CommonModule } from '@angular/common';
 import { AboutComponent } from '../../components/about/about.component';
+import { ExperienceComponent } from '../../components/experience/experience.component';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, PhotoComponent, CommonModule, AboutComponent],
+  imports: [HeaderComponent, PhotoComponent, CommonModule, AboutComponent, ExperienceComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
