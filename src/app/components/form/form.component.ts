369830ebb63c12989727753f6acb0ca9c510d770
LucasAlberto0 <lucasalberto4321@gmail.com>
2024-08-27 18:41:44 -0300

    feat: form-component

---
+++ src/app/components/form/form.component.ts
@@ -0,0 +1,13 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+@Component({
+  selector: 'app-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class FormComponent {
+
+}
