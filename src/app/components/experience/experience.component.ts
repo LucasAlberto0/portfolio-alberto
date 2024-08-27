04ab36cc9bfb0d9fef24a56e8664623a6accdaab
LucasAlberto0 <lucasalberto4321@gmail.com>
2024-08-27 12:06:17 -0300

    style: experience-page-responsive-and-icons

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { Photo2Component } from '../photo2/photo2.component';
 
 @Component({
@@ -6,7 +6,8 @@ import { Photo2Component } from '../photo2/photo2.component';
   standalone: true,
   imports: [Photo2Component],
   templateUrl: './experience.component.html',
-  styleUrl: './experience.component.scss'
+  styleUrl: './experience.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class ExperienceComponent {
 
