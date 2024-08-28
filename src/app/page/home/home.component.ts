ff72988923b739214e4231163e193ec5a8352612
LucasAlberto0 <lucasalberto4321@gmail.com>
2024-08-27 21:43:42 -0300

    fix: header-hover

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -6,12 +6,13 @@ import { AboutComponent } from '../../components/about/about.component';
 import { ExperienceComponent } from '../../components/experience/experience.component';
 import { SkillsComponent } from '../../components/skills/skills.component';
 import { ProjectsComponent } from '../../components/projects/projects.component';
+import { FormComponent } from '../../components/form/form.component';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, PhotoComponent, CommonModule, AboutComponent, ExperienceComponent, SkillsComponent, ProjectsComponent],
+  imports: [HeaderComponent, PhotoComponent, CommonModule, AboutComponent, ExperienceComponent, SkillsComponent, ProjectsComponent, FormComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
