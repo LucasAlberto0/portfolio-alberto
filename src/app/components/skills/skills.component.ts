a87837d30c73b096d2236ed4d6c908b4813a33b3
LucasAlberto0 <lucasalberto4321@gmail.com>
2024-11-07 02:01:14 -0300

    test: bug-animation-skills

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,5 +1,5 @@
-import { CommonModule, isPlatformBrowser } from '@angular/common';
-import { Component, CUSTOM_ELEMENTS_SCHEMA, AfterViewInit, Inject, PLATFORM_ID, OnDestroy } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
@@ -9,41 +9,7 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA, AfterViewInit, Inject, PLATFORM_ID, 
   styleUrls: ['./skills.component.scss'],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class SkillsComponent implements AfterViewInit, OnDestroy {
+export class SkillsComponent  {
 
-  private intervalId: any;
-
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
-
-  ngAfterViewInit() {
-    if (isPlatformBrowser(this.platformId)) {
-      const slideWrapper = document.querySelector('.slide-wrapper') as HTMLElement;
-
-      if (slideWrapper) {
-        const slides = Array.from(document.querySelectorAll('.slide')) as HTMLElement[];
-        const totalSlides = slides.length;
-        const slideWidth = slides[0].offsetWidth;
-        const spacing = parseInt(window.getComputedStyle(slides[0]).marginRight, 10);
-        const totalWidth = (slideWidth + spacing) * totalSlides;
-
-        for (let i = 0; i < 2; i++) {
-          slides.forEach(slide => {
-            const clone = slide.cloneNode(true) as HTMLElement;
-            slideWrapper.appendChild(clone);
-          });
-        }
-
-        slideWrapper.style.width = `${totalWidth * 2}px`;
-
-        // Remove the interval since we're handling the infinite loop with CSS
-        // No need to toggle the 'reverse' class; the animation will loop infinitely
-      }
-    }
-  }
-
-  ngOnDestroy() {
-    if (this.intervalId) {
-      clearInterval(this.intervalId);
-    }
-  }
+  
 }
