075a34a8e036669becb04e2872736505cc84d888
LucasAlberto0 <lucasalberto4321@gmail.com>
2025-04-23 23:14:39 -0300

    fix: form-spam

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,13 +1,14 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class FormComponent {
-
+    mathAnswer = ''; 
 }
