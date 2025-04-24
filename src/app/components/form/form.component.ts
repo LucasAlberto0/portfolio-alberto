8eff8353742f959fa1c21a9aefdfe68e2f834dc6
LucasAlberto0 <lucasalberto4321@gmail.com>
2025-04-23 23:52:24 -0300

    fix: remove-ts-form

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,14 +1,13 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [FormsModule],
+  imports: [],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class FormComponent {
-    mathAnswer = ''; 
+
 }
